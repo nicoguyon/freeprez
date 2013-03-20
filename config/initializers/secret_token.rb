a80394c488a9add81eaffd1121732ee59a1a8f9b
Nicolas Guyon <nicoguyon@gmail.com>
2013-03-20 21:37:28 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeprez::Application.config.secret_token = 'ebfde872b41f734e2ed82661b1c131e870b148a51c7efed909771bcee1c960d159d5f6ecb9c50597f1890f7f449fdf44816f140db5708528407435c320e9706d'
